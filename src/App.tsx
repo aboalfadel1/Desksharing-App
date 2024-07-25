44b35d79c4f1b50b358604d86979b7a31e26db9e
Mouayad Saad <medosaad18@gmail.com>
2024-07-25 11:13:44 +0200

    App in github hochladen

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from '@mui/material'
+import './App.css'
+import DevicesOtherIcon from '@mui/icons-material/DevicesOther';
+function App() {
+ 
+
+  return (
+    <>
+        <Button variant="contained"> <DevicesOtherIcon /> Desksharing</Button>
+    </>
+  )
+}
+
+export default App
